6446887de8ca9eecf1ebf4317cee80d40f078c09
victorEric <victoreric1409@gmail.com>
2020-11-06 15:27:26 +0700

    CRUD selesai

--- todolist/list/urls.py
+++ todolist/list/urls.py
@@ -4,6 +4,8 @@ from  .import views
 app_name='mysite'
 
 urlpatterns=[
+    url('del/(?P<delete_id>[0-9]+)', views.delete, name='delete'),
+    url('update/(?P<update_id>[0-9]+)', views.update, name='update'),
     url('create/', views.create, name='create'),
-    url('/',views.list, name='index'),
+    url('',views.list, name='index'),
 ]
--- todolist/list/views.py
+++ todolist/list/views.py
@@ -3,20 +3,57 @@ from .models import List
 from .forms import ListForm
 from django.contrib import messages
 
+
+def delete(request, delete_id):
+    List.objects.filter(id=delete_id).delete()
+    return redirect('list:create')
+
+def update(request, update_id):
+    item_update = List.objects.get(id=update_id)
+    data = {
+        'item' : item_update.item,
+        'completed' : item_update.completed,
+    }
+
+    item_form = ListForm(request.POST or None, initial = data, instance = item_update)
+
+    if request.method == 'POST' :
+        if item_form.is_valid():
+            item_form.save()
+        return redirect ('list:create')
+
+    context = {
+        'Title':'Update',
+        "item_form" : item_form,
+    }
+    return render (request, 'list/update.html', context)
+
+def create(request):
+    all_items = List.objects.all()
+    item_form = ListForm(request.POST or None)
+
+    if request.method == 'POST' :
+        if item_form.is_valid():
+            item_form.save()
+            return redirect ('list/create.html')
+
+    context = {
+        "Title" : 'Create',
+        'all_items' : all_items,
+        "item_form" : item_form,
+    }
+    return render (request, 'list/create.html', context)
+
 def list(request):
-    all_items = List.objects.all 
+    all_items = List.objects.all() 
 
     context = {
         'Title' : 'List',
-        'all_items' : all_items
+        'all_items' : all_items,
     }
     return render (request, 'list/index.html', context)
 
-def create(request):
-    context = {
-        'Title' : 'Setting'
-    }
-    return render (request, 'list/create.html', context)
+
 
 
 
