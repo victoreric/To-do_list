e4ed424b4fe9d09b25b8d7df7c68f6d650e3a475
victorEric <victoreric1409@gmail.com>
2020-11-11 23:10:16 +0700

    added cov19tomsk, cov19allregion

--- todolist/mysite/views.py
+++ todolist/mysite/views.py
@@ -28,9 +28,35 @@ def index(request):
 
     update = datetime.now()-timedelta(days=1)
 
+    response2= requests.get("https://api.apify.com/v2/key-value-stores/1brJ0NLbQaJKPTWMO/records/LATEST?disableRedirect=true")
+
+    datarusia= response2.json()
+    infectedByRegion=(datarusia['infectedByRegion'])
+    tomsk = (infectedByRegion[32])
+    case3 = []
+    result3 = {}
+    result3['region'] = tomsk['region']
+    result3['isoCode'] = tomsk['isoCode']
+    result3['infected'] = tomsk['infected']
+    result3['recovered'] = tomsk['recovered']
+    result3['deceased'] = tomsk['deceased']
+    case3.append(result3)  
+
+    case4 =[]
+    for reg in infectedByRegion:
+        result4={}
+        result4['region'] = reg['region']
+        result4['infected'] = reg['infected']
+        result4['recovered'] = reg['recovered'] 
+        result4['deceased'] = reg['deceased']
+        case4.append(result4)
+
+
     context = {
         'Title' : 'Home',
         'case2' : case2,
+        'case3' : case3,
+        'case4': case4,
         'update' : update,
     }
     return render(request,'index.html', context)
